55d684cc05ba5160b0e0a21a9f169458e5628d8c
Matt Chun-Lum <matt@questionable.engineering>
2023-07-04 10:32:08 -0700

    feat: add msrv script

---
+++ scripts/check-msrv.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+set -ex
+
+# We need to work around a current limitaiton in `cargo msrv` where it can't
+# handle verifying the version specified by a workspace.
+# https://github.com/foresterre/cargo-msrv/issues/590
+#
+# So what we're going to do is determine the minimum version that's declared in
+# the Cargo.toml at the root of the repository and explicitly constrain
+# `cargo msrv` to that version using `--min` and `--max`
+
+rust_version=$(grep 'rust-version' Cargo.toml | cut -d '"' -f 2)
+
+# just a basic check
+cargo msrv --min=${rust_version} --max=${rust_version}
+
+# verify we can run tests at this version
+cargo msrv --min=${rust_version} --max=${rust_version} -- cargo test --all-targets
