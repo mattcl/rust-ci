960bdee9813150c2f81ee05f1575826bd3eadddf
Matt Chun-Lum <matt@questionable.engineering>
2023-07-04 16:25:50 -0700

    feat: fmt and linting actions

--- scripts/lint-and-test.sh
+++ scripts/lint-and-test.sh
@@ -1,5 +1,14 @@
 #!/bin/sh
 set -ex
 
+# make sure we're formatted
+cargo fmt --check
+
+# fail on clippy warnings
+cargo clippy -Dwarnings
+
+# ensure we can build
 cargo build --verbose ${EXTRA_CARGO_BUILD_FLAGS}
+
+# ensure tests pass
 cargo test --verbose ${EXTRA_CARGO_TEST_FLAGS}
