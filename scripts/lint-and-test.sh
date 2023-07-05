2a12505ed5ef1e720bd4865243c5bfff2f052555
Matt Chun-Lum <matt@questionable.engineering>
2023-07-04 17:05:06 -0700

    fix: don't lint on nightly

--- scripts/lint-and-test.sh
+++ scripts/lint-and-test.sh
@@ -1,11 +1,13 @@
 #!/bin/sh
 set -ex
 
-# make sure we're formatted
-cargo fmt --check
+if [ "$LINT" -eq 1 ]; then
+    # make sure we're formatted
+    cargo fmt --check
 
-# fail on clippy warnings
-cargo clippy -- -Dwarnings
+    # fail on clippy warnings
+    cargo clippy -- -Dwarnings
+fi
 
 # ensure we can build
 cargo build --verbose ${EXTRA_CARGO_BUILD_FLAGS}
