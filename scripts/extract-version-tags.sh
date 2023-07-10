c3bcacb3079e8fdb1466c4c248a057bc9e2fb903
Matt Chun-Lum <matt@questionable.engineering>
2023-07-10 01:38:39 -0700

    fix: attempt to force root user for curl container

--- scripts/extract-version-tags.sh
+++ scripts/extract-version-tags.sh
@@ -8,13 +8,12 @@ docker_tag_exists() {
     curl --silent -f --head -lL https://hub.docker.com/v2/repositories/$1/tags/$2/ > /dev/null
 }
 
-cd image-tags
 if docker_tag_exists "$IMAGE_REPO" "$VERSION"; then
     echo "Version ${VERSION} already exists. Not including it in tags."
-    echo "dev" > tags
+    echo "dev" > image-tags/tags
 else
     echo "Version ${VERSION} not detected in remote. Including it in the tags."
-    echo "${VERSION} dev" > tags
+    echo "${VERSION} dev" > image-tags/tags
 fi
 
-cat tags
+cat image-tags/tags
