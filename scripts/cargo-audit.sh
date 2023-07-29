e5b3351f33d4ccdc7641f2d1f1d30f350d92b42f
Matt Chun-Lum <matt@questionable.engineering>
2023-07-29 14:25:50 -0700

    feat: ignore chrono security advisory.
    https://github.com/chronotope/chrono/issues/1073

--- scripts/cargo-audit.sh
+++ scripts/cargo-audit.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 set -ex
 
-cargo audit
+# TODO: remove this once a new version of chrono comes out. This advisory has
+# been open for a while now.
+cargo audit --ignore RUSTSEC-2020-0071
