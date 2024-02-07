dc7a2aaaac41fd56b65fc495ea1b5411242958ac
Matt Chun-Lum <matt@questionable.engineering>
2024-02-06 21:54:38 -0800

    chore: ignore RSA vuln until fix.
    https://github.com/RustCrypto/RSA/pull/394

--- scripts/cargo-audit.sh
+++ scripts/cargo-audit.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 set -ex
 
-# TODO: remove this once a new version of chrono comes out. This advisory has
-# been open for a while now.
-cargo audit --ignore RUSTSEC-2020-0071
+# TODO: remove after https://github.com/RustCrypto/RSA/pull/394
+cargo audit --ignore RUSTSEC-2023-0071
